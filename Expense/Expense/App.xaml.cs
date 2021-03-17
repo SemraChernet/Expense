44004e33daf39ecb636502030f4d471054b120e6
SemraChernet <61530532+SemraChernet@users.noreply.github.com>
2021-03-17 00:04:14 -0700

    Add project files.

---
+++ Expense/Expense/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Expense
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new BudgetEntry();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+        internal class budgetFilename
+        {
+        }
+    }
+}
---
+++ Expense/Expense/BudgetEntry.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Expense
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BudgetEntry : ContentPage
+
+    {
+        string _filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Expense.txt");
+        public BudgetEntry()
+
+        {
+            InitializeComponent();
+        }
+
+
+        ////Go to Expense Entry page once budget is saved.
+        private void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+
+
+        }
+
+        //Make budget blank when user deletes budget
+        private void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+
+           
+        }
+
+    } 
+}
